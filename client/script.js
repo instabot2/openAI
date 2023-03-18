4ee08d26ae4564da916d336b0a1755c6718b4b06
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-18 21:29:39 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch('https://platform.openai.com/account/api-keys', {
+    const response = await fetch('https://api.openai.com/v1/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
