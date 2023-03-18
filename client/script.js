93ccfe0726f82ff98e43146e5c28dd8949aa0dc8
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-18 20:49:37 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,52 +1,52 @@
-import bot from './assets/bot.svg'
-import user from './assets/user.svg'
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
-const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
 
-let loadInterval
+let loadInterval;
 
 function loader(element) {
-  element.textContent = ''
+  element.textContent = '';
 
   loadInterval = setInterval(() => {
     // Update the text content of the loading indicator
-    element.textContent += '.'
+    element.textContent += '.';
 
     // If the loading indicator has reached three dots, reset it
     if (element.textContent === '....') {
-      element.textContent = ''
+      element.textContent = '';
     }
-  }, 300)
+  }, 300);
 }
 
 function typeText(element, text) {
-  let index = 0
+  let index = 0;
 
   let interval = setInterval(() => {
     if (index < text.length) {
-      element.innerHTML += text.charAt(index)
-      index++
+      element.innerHTML += text.charAt(index);
+      index++;
     } else {
-      clearInterval(interval)
+      clearInterval(interval);
     }
-  }, 20)
+  }, 20);
 }
 
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
 // without unique ID, typing text will work on every element
 function generateUniqueId() {
-  const timestamp = Date.now()
-  const randomNumber = Math.random()
-  const hexadecimalString = randomNumber.toString(16)
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
 
-  return `id-${timestamp}-${hexadecimalString}`
+  return `id-${timestamp}-${hexadecimalString}`;
 }
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-        <div class="wrapper ${isAi && 'ai'}">
+        <div class="wrapper ${isAi ? 'ai' : ''}">
             <div class="chat">
                 <div class="profile">
                     <img 
@@ -57,64 +57,70 @@ function chatStripe(isAi, value, uniqueId) {
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
         </div>
-    `
+    `;
 }
 
 const handleSubmit = async (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
-  const data = new FormData(form)
+  const data = new FormData(form);
 
   // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
 
-  // to clear the textarea input 
-  form.reset()
+  // to clear the textarea input
+  form.reset();
 
   // bot's chatstripe
-  const uniqueId = generateUniqueId()
-  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId)
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
 
-  // to focus scroll to the bottom 
-  chatContainer.scrollTop = chatContainer.scrollHeight
+  // to focus scroll to the bottom
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  // specific message div 
-  const messageDiv = document.getElementById(uniqueId)
+  // specific message div
+  const messageDiv = document.getElementById(uniqueId);
 
   // messageDiv.innerHTML = "..."
-  loader(messageDiv)
+  loader(messageDiv);
 
   try {
-    const response = await fetch('https://chatgpt-ai-lujs.onrender.com', {
+    const response = await fetch('https://api.openai.com/v1/completions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${process.env.OPENAI_API_KEY}'
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
       },
       body: JSON.stringify({
-        prompt: data.get('prompt')
+        model: 'text-davinci-003',
+        prompt: data.get('prompt'),
+        temperature: 0,
+        max_tokens: 3000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0
       })
-    })
-   
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = ' '
+    });
+
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = ' ';
 
     if (response.ok) {
-      const data = await response.json()
-      const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+      const data = await response.json();
+      const parsedData = data.choices[0].text.trim(); // trims any trailing spaces/'\n'
 
-      typeText(messageDiv, parsedData)
+      typeText(messageDiv, parsedData);
     } else {
-      const err = await response.text()
-      messageDiv.innerHTML = `Error: ${err}`
-      console.error(err)
+      const err = await response.text();
+      messageDiv.innerHTML = `Error: ${err}`;
+      console.error(err);
     }
   } catch (err) {
-    clearInterval(loadInterval)
-    messageDiv.innerHTML = `Error: ${err.message}`
-    console.error(err)
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = `Error: ${err.message}`;
+    console.error(err);
   }
-}
+};
 
 form.addEventListener('submit', handleSubmit)
 
@@ -123,3 +129,4 @@ form.addEventListener('keyup', (e) => {
     handleSubmit(e)
   }
 })
+
