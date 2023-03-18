76621ca267fbeeafb57ea7ec15affb573e4f9625
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-18 15:14:37 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,52 +1,48 @@
-const express = require('express');
-const OpenAI = require('openai');
-const cors = require('cors');
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+dotenv.config()
 
-// Initialize OpenAI API with static API key
-const openai = new OpenAI('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');
-
-// Route for the homepage
-app.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Hello from ChatGPT AI, server is running successfully.',
-  });
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-// Route for generating response to user prompt
-app.post('/', async (req, res, next) => {
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
-    const response = await openai.complete({
-      engine: 'text-davinci-003',
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0,
-      maxTokens: 3000,
-      topP: 1,
-      frequencyPenalty: 0.5,
-      presencePenalty: 0,
-      stop: '\n',
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
+
     res.status(200).send({
-      bot: response.choices[0].text,
+      bot: response.data.choices[0].text
     });
+
   } catch (error) {
-    console.error(error);
-    next(error);
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
   }
-});
+})
 
-// Error handler
-app.use((err, req, res, next) => {
-  res.status(500).send({
-    message: err.message || 'Something went wrong',
-    error: err,
-  });
-});
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`ChatGPT server started on http://localhost:${PORT}`));
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
