373703d8a778353f6e5cc9be17678977ec11bcd4
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-19 14:31:27 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,15 @@
 import express, { Request, Response, NextFunction } from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import * as OpenAI from 'ai.text'
 
 dotenv.config()
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+OpenAI.apiKey = process.env.OPENAI_API_KEY
 
 app.get('/', async (req, res) => {
   res.status(200).send({
@@ -25,18 +21,22 @@ app.post('/', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
+    const response = await OpenAI.complete({
+      model: 'text-davinci-002',
       prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      temperature: 0.7,
+      maxTokens: 100,
+      n: 1,
+      stop: null,
+      stream: false,
+      presencePenalty: 0,
+      frequencyPenalty: 0,
+      bestOf: 1,
+      apiKey: process.env.OPENAI_API_KEY,
     });
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: response.choices[0].text
     });
 
   } catch (error) {
