14df3e6e4789d8d859816be5cb891f80f706407a
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-19 15:00:06 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
 
-dotenv.config()
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,38 +15,58 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
-  })
-})
+    message: 'Hello from ChatGPT AI!',
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const { prompt } = req.body;
+
+    if (!prompt) {
+      throw new Error('Missing prompt field');
+    }
 
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      model: 'text-davinci-003',
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
     });
 
-    res.status(200).send({
-      bot: response.data.choices[0].text
-    });
+    const botResponse = response.data.choices[0].text;
 
+    res.status(200).send({ bot: botResponse });
   } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
-  }
-})
+    console.error(error);
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+    if (error.response) {
+      const { status, data } = error.response;
+
+      if (status === 401) {
+        res.status(401).send({ error: 'Unauthorized' });
+      } else if (status === 404) {
+        res.status(404).send({ error: 'Not found' });
+      } else {
+        res.status(status).send({ error: data });
+      }
+    } else {
+      res.status(500).send({ error: error.message || 'Something went wrong' });
+    }
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send({ error: err.message || 'Something went wrong' });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
