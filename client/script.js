f695fb19f076373bdf631367372ac1566e112d22
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-20 14:36:51 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://mychatgpt-app.onrender.com', {
+    const response = await fetch('https://chatgpt-ai-lujs.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -97,7 +97,7 @@ const handleSubmit = async (e) => {
     })
 
     clearInterval(loadInterval)
-    messageDiv.innerHTML = " "
+    messageDiv.innerHTML = ""
 
     if (response.ok) {
         const data = await response.json();
