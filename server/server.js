0f798bd61ee7c8a2ad5eebeef827ec3cc471ba8d
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-21 09:41:39 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
