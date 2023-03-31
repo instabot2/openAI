e00b5aa3215a8d0c5ca6a13b26830afff6d61ede
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-03-31 17:54:05 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -12,7 +12,6 @@ function loader(element) {
   loadInterval = setInterval(() => {
     // Update the text content of the loading indicator
     element.textContent += '.';
-
     // If the loading indicator has reached three dots, reset it
     if (element.textContent === '....') {
       element.textContent = '';
