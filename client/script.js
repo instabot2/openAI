a93b27b18c3f206b136e7a1a4ca48843bd6f5cdc
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-01 17:36:01 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -54,10 +54,12 @@ function chatStripe(isAi, value, uniqueId) {
   `;
 }
 
+
 const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
+  
   // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
   // to clear the textarea input
