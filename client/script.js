dedb9e812c239a7194c850da78aeebcfd1e30a3b
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-02 15:12:36 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -59,32 +59,32 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
-  
+
   // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
   // to clear the textarea input
   form.reset();
   // bot's chatstripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
   // specific message div
   const messageDiv = document.getElementById(uniqueId);
   // messageDiv.innerHTML = '...'
   loader(messageDiv);
 
   try {
-    const response = await fetch('https://chatgpt-ai-lujs.onrender.com', {
-      method: 'POST',
+    const response = await fetch("https://chatgpt-ai-lujs.onrender.com", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        prompt: data.get('prompt'),
+        prompt: data.get("prompt"),
       }),
     });
 
     clearInterval(loadInterval);
-    messageDiv.innerHTML = '';
+    messageDiv.innerHTML = "";
 
     if (response.ok) {
       const data = await response.json();
@@ -99,10 +99,8 @@ const handleSubmit = async (e) => {
     console.error(err);
   }
 
-  // scroll to top when new message arrives
+  // scroll to the top of chat container
   chatContainer.scrollTop = 0;
-  // focus scroll to the bottom again
-  //chatContainer.scrollTop = chatContainer.scrollHeight;
 };
 
 
