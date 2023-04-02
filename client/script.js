e6f375ca694d7ec006e73c98c8cbe14ec3d1cbf0
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-02 13:49:13 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -40,7 +40,61 @@ typeText(messageDiv, parsedData, () => {
   chatContainer.scrollTop = messageDiv.offsetTop;
 });
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
 
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat">
+        <div class="profile">
+          <img src=${isAi ? bot : user} alt="${isAi ? 'bot' : 'user'}"/>
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>
+  `;
+}
+
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+  loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
+    element.textContent += '.';
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
 
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
@@ -119,6 +173,31 @@ const handleSubmit = async (e) => {
   chatContainer.scrollTop = chatContainer.scrollHeight;
 };
 
+
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
+
+window.addEventListener('resize', () => {
+  // check if user has scrolled up before resizing
+  const isScrolledToBottom = chatContainer.scrollHeight - chatContainer.clientHeight <= chatContainer.scrollTop + 1;
+  // calculate new scroll position after resizing
+  const newScrollTop = (chatContainer.scrollHeight - chatContainer.clientHeight) * (chatContainer.scrollTop / (chatContainer.scrollHeight - chatContainer.clientHeight));
+  // set chat container height to its current height
+  chatContainer.style.height = `${chatContainer.clientHeight}px`;
+  // scroll to the bottom of the chat container if user is already at the bottom
+  if (isScrolledToBottom) {
+    chatContainer.scrollTop = newScrollTop;
+  }
+  // if user has scrolled up, keep their scroll position after resizing
+  else {
+    chatContainer.scrollTop = chatContainer.scrollHeight - chatContainer.clientHeight - (1 - (newScrollTop / chatContainer.clientHeight)) * (chatContainer.scrollHeight - chatContainer.clientHeight);
+  }
+});
 
 
 form.addEventListener('submit', handleSubmit);
