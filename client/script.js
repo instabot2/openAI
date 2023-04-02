f39b2abdf34d204f1acad1b88107371ab7adfd12
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-02 12:30:34 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -58,32 +58,32 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
-
+  
   // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
   // to clear the textarea input
   form.reset();
   // bot's chatstripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  chatContainer.innerHTML += chatStripe(true, ' ', uniqueId);
   // specific message div
   const messageDiv = document.getElementById(uniqueId);
   // messageDiv.innerHTML = '...'
   loader(messageDiv);
 
   try {
-    const response = await fetch("https://chatgpt-ai-lujs.onrender.com", {
-      method: "POST",
+    const response = await fetch('https://chatgpt-ai-lujs.onrender.com', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        prompt: data.get("prompt"),
+        prompt: data.get('prompt'),
       }),
     });
 
     clearInterval(loadInterval);
-    messageDiv.innerHTML = "";
+    messageDiv.innerHTML = '';
 
     if (response.ok) {
       const data = await response.json();
@@ -91,6 +91,9 @@ const handleSubmit = async (e) => {
       typeText(messageDiv, parsedData);
       // scroll to the latest message
       messageDiv.scrollIntoView();
+
+      // scroll up to the new message and display it on top of the browser
+      chatContainer.scrollTop = messageDiv.offsetTop;
     } else {
       const err = await response.text();
       messageDiv.innerHTML = `Error: ${err}`;
@@ -100,15 +103,10 @@ const handleSubmit = async (e) => {
     console.error(err);
   }
 
-  // scroll up to the new message and display it on top of the browser
-  chatContainer.scrollBy({
-    top: -10000,
-    behavior: "smooth"
-  });
-
   // focus scroll to the bottom again
   chatContainer.scrollTop = chatContainer.scrollHeight;
 };
+
 
 
 
