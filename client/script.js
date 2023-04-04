283c6cef8057e74d04fa31c75d491238905c2b45
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-04 09:39:00 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -108,6 +108,10 @@ const handleSubmit = async (e) => {
       const data = await response.json();
       const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
       typeText(messageDiv, parsedData, () => {
+        // Store the message in local storage
+        messages.push({ isBot: true, message: parsedData });
+        localStorage.setItem('messages', JSON.stringify(messages));
+
         // scroll up to the new message and display it on top of the browser
         const messageDivHeight = messageDiv.offsetHeight;
         const previousMessageDivsHeight = Array.from(messageWrapper.children).reduce((acc, cur) => acc + cur.offsetHeight, 0);
@@ -116,10 +120,6 @@ const handleSubmit = async (e) => {
         chatContainer.scrollTop = 0;
         // scroll to the new message
         scrollIntoView(messageDiv);
-
-        // Store the message in local storage
-        messages.push({ isBot: true, message: parsedData });
-        localStorage.setItem('messages', JSON.stringify(messages));
       });
     } else {
       const err = await response.text();
@@ -137,15 +137,6 @@ const handleSubmit = async (e) => {
       chatContainer.scrollTop = 0;
     }
   });
-
-  // scroll up to the new message and display it on top of the browser
-  const messageDivHeight = messageDiv.offsetHeight;
-  const previousMessageDivsHeight = Array.from(messageWrapper.children).reduce((acc, cur) => acc + cur.offsetHeight, 0);
-  chatContainer.scrollTop = previousMessageDivsHeight + messageDivHeight - chatContainer.offsetHeight;
-  // scroll to the latest message
-  chatContainer.scrollTop = 0;
-  // scroll to the new message
-  scrollIntoView(messageDiv);
 
   // Store the user's message in local storage
   messages.push({ isBot: false, message: data.get('prompt') });
