b2322f3dca44ab89004006a3e33255ed493b8c0a
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-07 15:04:01 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -98,6 +98,9 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
+  // scroll to the top of the chat container to show the new message
+  chatContainer.scrollTop = 0;
+  
   try {
     const response = await fetch('https://chatgpt-ai-lujs.onrender.com', {
       method: 'POST',
@@ -111,9 +114,6 @@ const handleSubmit = async (e) => {
 
     clearInterval(loadInterval);
     messageDiv.innerHTML = '';
-
-    // scroll to the top of the chat container to show the new message
-    chatContainer.scrollTop = 0;
     
     if (response.ok) {
       const data = await response.json();
