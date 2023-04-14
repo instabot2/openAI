ae97c6a16d40bb7acb367c4f19b2aee4923965b2
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-14 16:39:08 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -79,7 +79,6 @@ let conversationHistory = [];
 const updateConversationHistory = (newConversationHistory) => {
   conversationHistory = newConversationHistory;
   window.alert(`conversationHistory: ${conversationHistory}`);
-  saveConversationHistory(); // Save conversation history to local storage after every update
 };
 
 // Load conversation history from local storage
@@ -91,13 +90,13 @@ const loadConversationHistory = () => {
   }
 };
 
+// Call loadConversationHistory before handleSubmit
 loadConversationHistory();
 
 // Function to save conversation history to local storage
 const saveConversationHistory = () => {
   window.localStorage.setItem('conversationHistory', JSON.stringify(conversationHistory));
 };
-
 
 
 // handleSubmit function
@@ -172,12 +171,14 @@ const handleSubmit = async (e) => {
         }
       });
 
-
       // Update conversationHistory with new data
-      updateConversationHistory(responseData.conversationHistory);
-      alert(`conversationHistory updated: ${JSON.stringify(conversationHistory)}`);
-
-      
+      if (responseData.conversationHistory) {
+        updateConversationHistory(responseData.conversationHistory);
+        alert(`conversationHistory updated: ${JSON.stringify(conversationHistory)}`);
+      } else {
+        alert(`conversationHistory not updated. responseData: ${JSON.stringify(responseData)}`);
+      }
+    
     } else {
       console.error(`Response status: ${response.status}`);
     }
