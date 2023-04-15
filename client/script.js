0c5baa24df32a468cc7e0b8a954a7c28bb445788
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-15 11:17:18 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ function chatStripe(isAi, value, uniqueId) {
 
 
 // Retrieve stored messages from local storage
-let oldMessages = JSON.parse(localStorage.getItem('messages')) || [];
+//let oldMessages = JSON.parse(localStorage.getItem('messages')) || [];
 
 // Define the conversationHistory variable before using it in handleSubmit
 let conversationHistory = oldMessages.map((message) => ({ text: message.message }));
