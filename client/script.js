33bec1dfe994db5fc2885133fffc8c31e920d655
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-17 17:17:35 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -259,6 +259,8 @@ textarea.addEventListener("input", () => {
 
 // When the cursor is not in the textarea, hide the hidden_text div and remove the background blur
 document.addEventListener("click", (event) => {
+  document.body.classList.add("blur");
+  
   if (event.target !== textarea && event.target !== hiddenText) {
     hiddenText.style.display = "none";
     document.body.classList.remove("blur"); // remove the .blur class from the body element
