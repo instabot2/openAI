caa135325bc0d6d1fba4306c6e4fbad38520478b
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-20 12:35:53 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -201,9 +201,10 @@ const handleSubmit = async (e) => {
       
       // Display an error message to the user
       if (response.status === 400) {
-        const errorMessage = `
-          <div style="background-color: lightcoral; padding: 10px;">
+        const errorMessage = `>
+          <div style="background-color: rgb(240, 128, 128); padding: 10px;">
             <p>Oops! Looks like you've hit the usage limit of the free OpenAI API key. Please upgrade to a paid plan to continue using the service.</p>
+            <p>Note: Refreshing the page will erase all memory and start a new conversation.</p>
             <button id="refresh-button">Refresh</button>
           </div>
         `;
