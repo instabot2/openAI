c36fbe5fc6f0b87036d344f39dd869cdf1b824d7
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-04-28 13:27:32 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -274,7 +274,7 @@ async function crawlData(conversationHistory, prompt) {
     window.alert(`searchResults: ${searchResults}`);
   } catch (error) {
     console.error('Error crawling data:', error);
-    window.alert(`Error crawling data`);
+    window.alert('Error crawling data:', error.message);
     return null; // Handle error cases appropriately
   }
 }
