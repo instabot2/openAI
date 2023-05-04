0eee51bd9d1909b79a168a6efd2399f4dd24bd08
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-04 16:00:06 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -106,6 +106,36 @@ function chatStripe(isAi, value, uniqueId) {
 
 let conversationHistory = [];
 
+
+// Define the crawlData function
+async function crawlData(conversationHistory, prompt) {
+  try {
+    const query = `${conversationHistory.map((msg) => msg.message).join('\n')}\n${prompt}`;
+    const response = await fetch(`https://www.google.com/search?q=${encodeURIComponent(query)}`);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const searchResults = [];
+    $('.tF2Cxc').each((index, element) => {
+      const titleElement = $(element).find('h3');
+      const linkElement = $(element).find('a');
+      const descriptionElement = $(element).find('.aCOpRe');
+      if (titleElement && linkElement && descriptionElement) {
+        const title = $(titleElement).text();
+        const link = $(linkElement).attr('href');
+        const description = $(descriptionElement).text();
+        searchResults.push({ title, link, description });
+      }
+    });
+    window.alert(`Search Results:\n\n${JSON.stringify(searchResults, null, 2)}`);
+    return searchResults;
+  } catch (error) {
+    console.error('Error crawling data:', error);
+    window.alert('An error occurred while crawling data.');
+    return null; // Handle error cases appropriately
+  }
+}
+
+
 const handleSubmit = async (e) => {
   e.preventDefault();
 
@@ -151,6 +181,14 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
+  
+    // Call crawlData function
+    const searchResults = await crawlData(conversationHistory, data.get('prompt'));
+    if (searchResults) {
+      console.log('Search results:', searchResults);
+      // Handle the search results as needed
+    }
+  
     const response = await fetch('https://chatgpt-ai-lujs.onrender.com', {
       method: 'POST',
       headers: {
