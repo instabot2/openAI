a5b927c3238081dae4132fa42aabaecade2cef6d
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-04 15:13:09 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -255,8 +255,8 @@ function handleRefresh() {
 }
 
 
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+//const fetch = require('node-fetch');
+//const cheerio = require('cheerio');
 async function crawlData(conversationHistory, prompt) {
   try {
     const query = `${conversationHistory.map((msg) => msg.message).join('\n')}\n${prompt}`;
