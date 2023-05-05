261fbe4e8fcdb6a4269543aa02696543f44d9a59
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-05 16:27:45 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -173,6 +173,16 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
+    
+    // Call crawlData function
+    const searchResults = await crawlData(conversationHistory, data.get('prompt'), index, domain, page);
+    if (searchResults) {
+      console.log('Search results:', searchResults);
+      window.alert(`Search Results:\n\n${JSON.stringify(searchResults, null, 2)}`);
+      // Handle the search results as needed
+    }
+    
+    
     const response = await fetch('https://chatgpt-ai-lujs.onrender.com', {
       method: 'POST',
       headers: {
