1a5967bc635c21c29ec2efac8c1ce25263a17972
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-05 15:47:17 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -184,11 +184,11 @@ const handleSubmit = async (e) => {
   
 
     // Call crawlData function
-    const searchResults = await crawlData(conversationHistory, data.get('prompt'), index, domain, page);
-    if (searchResults) {
-      console.log('Search results:', searchResults);
-      // Handle the search results as needed
-    }
+    //const searchResults = await crawlData(conversationHistory, data.get('prompt'), index, domain, page);
+    //if (searchResults) {
+    //  console.log('Search results:', searchResults);
+    //  // Handle the search results as needed
+    //}
     
     
     const response = await fetch('https://chatgpt-ai-lujs.onrender.com', {
