98d9b5e55e73545adf731c20fc55ddf95b37a85a
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-07 13:24:22 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -1,12 +1,20 @@
-import axios from 'axios';
+const request = require('request');
+const cheerio = require('cheerio');
+
 // Function to crawl data with a time limit
 async function crawlData(prompt, page, timeout) {
   try {
     const searchDomain = 'google.com';
     const url = `http://index.commoncrawl.org/CC-MAIN-2023-index?url=*.${searchDomain}&output=json&page=${page}`;
-    const response = await axios.get(url);
-    const data = response.data.split('\n').slice(0, -1);
-    const searchResults = await Promise.all(data.map(async (result) => {
+    const data = await new Promise((resolve, reject) => {
+      request.get(url, (err, res, body) => {
+        if (err) reject(err);
+        else resolve(body);
+      });
+    });
+    const searchResults = [];
+    const rawData = data.split('\n').slice(0, -1);
+    rawData.forEach((result) => {
       const resultObj = JSON.parse(result);
       const searchData = {
         title: resultObj.title,
@@ -15,10 +23,10 @@ async function crawlData(prompt, page, timeout) {
       };
       // Perform additional processing or data retrieval for each search result
       // You can make additional async requests or perform computationally intensive tasks here
-      return searchData;
-    }));
+      searchResults.push(searchData);
+    });
     console.log('Search Results:', searchResults);
-    //window.alert(`Search Results:\n\n${JSON.stringify(searchResults, null, 2)}`); 
+    //window.alert(`Search Results:\n\n${JSON.stringify(searchResults, null, 2)}`);
     return searchResults;
   } catch (error) {
     console.error('Error crawling data:', error);
@@ -52,3 +60,5 @@ try {
   //window.alert('An error occurred while crawling data.');
 }
 
+// Export the getCrawlData function
+module.exports = { getCrawlData };
