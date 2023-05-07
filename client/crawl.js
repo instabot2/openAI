b003ba31af540a722918d0f0c5b17b524b2d4842
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-07 16:08:31 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -1,18 +1,35 @@
 const Parser = require('rss-parser');
 const parser = new Parser();
 
+// Define the getCrawlData function
+function getCrawlData(feedData, userInput) {
+  // do something with the feed data and user input
+  console.log(`Search results for "${userInput}":`, feedData);
+}
+
 (async () => {
   try {
+    // Get today's date
+    const today = new Date();
+    const date = today.getDate();
+    const month = today.getMonth() + 1; // January is 0
+    const year = today.getFullYear();
+    const formattedDate = `${year}-${month}-${date}`;
+
+    // RSS feed URL
+    const rssUrl = 'https://news.google.com/rss';
+
+    // Get user input from the data object
+    const userInput = `market news ${formattedDate}`;
+
+    // Log the URL to the console
+    console.log(`Fetching RSS feed from ${rssUrl}?q=${userInput}`);
+
     // Parse the RSS feed
-    const feed = await parser.parseURL('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml');
+    const feed = await parser.parseURL(`${rssUrl}?q=${userInput}`);
 
-    // Log the title of the RSS feed
-    console.log(`Title: ${feed.title}`);
-
-    // Log the title and link of each item in the RSS feed
-    feed.items.forEach(item => {
-      console.log(`${item.title}: ${item.link}`);
-    });
+    // Pass the RSS feed data and user input to the getCrawlData function
+    getCrawlData(feed, userInput);
   } catch (err) {
     console.error('There was an error fetching or parsing the RSS feed:', err);
   }
