1e2604e6d6b5c44398f73bd1f3312d7265802b64
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-07 16:03:05 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -1,13 +1,19 @@
-let Parser = require('rss-parser');
-let parser = new Parser();
+const Parser = require('rss-parser');
+const parser = new Parser();
 
 (async () => {
+  try {
+    // Parse the RSS feed
+    const feed = await parser.parseURL('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml');
 
-  let feed = await parser.parseURL('https://www.reddit.com/.rss');
-  console.log(feed.title);
+    // Log the title of the RSS feed
+    console.log(`Title: ${feed.title}`);
 
-  feed.items.forEach(item => {
-    console.log(item.title + ':' + item.link)
-  });
-
+    // Log the title and link of each item in the RSS feed
+    feed.items.forEach(item => {
+      console.log(`${item.title}: ${item.link}`);
+    });
+  } catch (err) {
+    console.error('There was an error fetching or parsing the RSS feed:', err);
+  }
 })();
