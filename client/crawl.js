abb61e74382003928d01b3223fa56839542095b5
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-07 14:28:17 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -2,7 +2,8 @@
 const rssUrl = 'https://news.google.com/rss';
 
 // Get user input from the data object
-const userInput = data.get('prompt');
+//const userInput = data.get('prompt');
+const userInput = "market news";
 
 // Log the URL to the console
 console.log(`Fetching RSS feed from ${rssUrl}?q=${userInput}`);
