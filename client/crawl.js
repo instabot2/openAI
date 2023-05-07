849c0fe08b7cc081de87316865af9f22d4719faf
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-07 15:47:47 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -1,38 +1,36 @@
 const Parser = require('rss-parser');
-
-// Get today's date
-const today = new Date();
-const date = today.getDate();
-const month = today.getMonth() + 1; // January is 0
-const year = today.getFullYear();
-const formattedDate = `${year}-${month}-${date}`;
-
-// RSS feed URL
-const rssUrl = 'https://news.google.com/rss/search?q=';
-
-// Get user input from the data object
-const userInput = `market news ${formattedDate}`;
-
-// Log the URL to the console
-console.log(`Fetching RSS feed from ${rssUrl}${userInput}`);
-
-// Fetch the RSS feed
 const parser = new Parser();
-parser.parseURL(`${rssUrl}${userInput}`, (err, feed) => {
-  if (err) {
-    console.error('There was an error fetching the RSS feed. Please try again later.', err);
-    alert(`Error fetching RSS feed: ${err}`);
-  } else {
-    // Pass the RSS feed data and user input to the getCrawlData function
-    getCrawlData(feed, userInput);
+
+(async () => {
+  try {
+    // Get today's date
+    const today = new Date();
+    const date = today.getDate();
+    const month = today.getMonth() + 1; // January is 0
+    const year = today.getFullYear();
+    const formattedDate = `${year}-${month}-${date}`;
+
+    // RSS feed URL
+    const rssUrl = 'https://news.google.com/rss';
+
+    // Get user input from the data object
+    const userInput = `market news ${formattedDate}`;
+
+    // Log the URL to the console
+    console.log(`Fetching RSS feed from ${rssUrl}?q=${userInput}`);
+
+    // Parse the RSS feed
+    const feed = await parser.parseURL(`${rssUrl}?q=${userInput}`);
+
+    // Log the title of the RSS feed
+    console.log(`Search results for "${userInput}": ${feed.title}`);
+
+    // Log the title and link of each item in the RSS feed
+    feed.items.forEach(item => {
+      console.log(`${item.title}: ${item.link}`);
+    });
+  } catch (err) {
+    console.error('There was an error fetching or parsing the RSS feed:', err);
+    alert(`Error fetching or parsing RSS feed: ${err}`);
   }
-});
-
-// Define the getCrawlData function
-function getCrawlData(feedData, userInput) {
-  // do something with the feed data and user input
-  console.log(`Search results for "${userInput}":`, feedData);
-}
-
-// Export the getCrawlData function
-module.exports = { getCrawlData };
+})();
