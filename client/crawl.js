753a6afc860518938a55236c4eb28ca0b52e6f29
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-07 15:53:54 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -1,36 +1,13 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
+let Parser = require('rss-parser');
+let parser = new Parser();
 
 (async () => {
-  try {
-    // Get today's date
-    const today = new Date();
-    const date = today.getDate();
-    const month = today.getMonth() + 1; // January is 0
-    const year = today.getFullYear();
-    const formattedDate = `${year}-${month}-${date}`;
 
-    // RSS feed URL
-    const rssUrl = 'https://news.google.com/rss';
+  let feed = await parser.parseURL('https://www.reddit.com/.rss');
+  console.log(feed.title);
 
-    // Get user input from the data object
-    const userInput = `market news ${formattedDate}`;
+  feed.items.forEach(item => {
+    console.log(item.title + ':' + item.link)
+  });
 
-    // Log the URL to the console
-    console.log(`Fetching RSS feed from ${rssUrl}?q=${userInput}`);
-
-    // Parse the RSS feed
-    const feed = await parser.parseURL(`${rssUrl}?q=${userInput}`);
-
-    // Log the title of the RSS feed
-    console.log(`Search results for "${userInput}": ${feed.title}`);
-
-    // Log the title and link of each item in the RSS feed
-    feed.items.forEach(item => {
-      console.log(`${item.title}: ${item.link}`);
-    });
-  } catch (err) {
-    console.error('There was an error fetching or parsing the RSS feed:', err);
-    alert(`Error fetching or parsing RSS feed: ${err}`);
-  }
 })();
