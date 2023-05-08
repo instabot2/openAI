50159ec05c1a37d96116eee0a9ef1a3e8e09b4af
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-08 11:36:30 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -4,9 +4,14 @@ const parser = new Parser();
 export async function getCrawlData() {
   try {
     const feed = await parser.parseURL('https://news.google.com/rss');
-    return feed.items;
+    if (feed && feed.items) {
+      return feed.items;
+    } else {
+      throw new Error('Invalid feed or missing items');
+    }
   } catch (err) {
-    console.error(err);
+    console.error(`Error fetching crawl data: ${err}`);
+    alert('An error occurred while fetching crawl data. Please try again later.');
     return [];
   }
 }
