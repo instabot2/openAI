e6bc970f9eff8bf19c540870850f4df441923b56
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-08 11:19:38 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -1,14 +1,14 @@
 const Parser = require('rss-parser');
 const parser = new Parser();
 
-(async () => {
+async function fetchData() {
   const crawlData = await getCrawlData();
   const feed = await parser.parseURL('https://www.reddit.com/.rss');
-
   console.log(crawlData);
   console.log(feed.title);
-
   feed.items.forEach(item => {
     console.log(item.title + ':' + item.link);
   });
-})();
+}
+
+fetchData();
