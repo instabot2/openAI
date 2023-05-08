5cd1c0a5e7c8e88595e3bd47ca20bfea725605ea
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-08 10:57:40 +0800

    Update crawl.js

--- client/crawl.js
+++ client/crawl.js
@@ -1,36 +1,13 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
-
-// Define the getCrawlData function
-function getCrawlData(feedData, userInput) {
-  // do something with the feed data and user input
-  console.log(`Search results for "${userInput}":`, feedData);
-}
+let Parser = require('rss-parser');
+let parser = new Parser();
 
 (async () => {
-  try {
-    // Get today's date
-    const today = new Date();
-    const date = today.getDate();
-    const month = today.getMonth() + 1; // January is 0
-    const year = today.getFullYear();
-    const formattedDate = `${year}-${month}-${date}`;
 
-    // RSS feed URL
-    const rssUrl = 'https://news.google.com/rss';
+  let feed = await parser.parseURL('https://www.reddit.com/.rss');
+  console.log(feed.title);
 
-    // Get user input from the data object
-    const userInput = `market news ${formattedDate}`;
+  feed.items.forEach(item => {
+    console.log(item.title + ':' + item.link)
+  });
 
-    // Log the URL to the console
-    console.log(`Fetching RSS feed from ${rssUrl}?q=${userInput}`);
-
-    // Parse the RSS feed
-    const feed = await parser.parseURL(`${rssUrl}?q=${userInput}`);
-
-    // Pass the RSS feed data and user input to the getCrawlData function
-    getCrawlData(feed, userInput);
-  } catch (err) {
-    console.error('There was an error fetching or parsing the RSS feed:', err);
-  }
 })();
