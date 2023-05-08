050ef679ebb04aa6d7601b2765f736ca966f19b5
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-08 13:36:50 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -20,61 +20,6 @@ function loader(element) {
 }
 
 
-
-// Import the fetchRSSFeed function from rss.js
-import { fetchRSSFeed } from './rss.js';
-
-// Define a function to render the RSS feed data in the DOM
-function renderRSSFeed(feed) {
-  // Get a reference to the container element in the DOM
-  const container = document.querySelector('.rss-container');
-
-  // Create a new element to display the feed title
-  const titleEl = document.createElement('h2');
-  titleEl.textContent = feed.title;
-  container.appendChild(titleEl);
-
-  // Create a new element to display the feed description
-  const descriptionEl = document.createElement('p');
-  descriptionEl.textContent = feed.description;
-  container.appendChild(descriptionEl);
-
-  // Create a new element to display each feed item
-  feed.items.forEach(item => {
-    const itemEl = document.createElement('div');
-
-    const titleLinkEl = document.createElement('a');
-    titleLinkEl.textContent = item.title;
-    titleLinkEl.href = item.link;
-    titleLinkEl.target = '_blank';
-    itemEl.appendChild(titleLinkEl);
-
-    const pubDateEl = document.createElement('p');
-    pubDateEl.textContent = item.pubDate;
-    itemEl.appendChild(pubDateEl);
-
-    const descriptionEl = document.createElement('p');
-    descriptionEl.textContent = item.description;
-    itemEl.appendChild(descriptionEl);
-
-    container.appendChild(itemEl);
-  });
-}
-
-// Define the URL of the RSS feed to fetch
-const feedUrl = 'https://www.msn.com/en-us/news/rss';
-
-// Fetch and parse the RSS feed using the fetchRSSFeed function
-fetchRSSFeed(feedUrl)
-  .then(feed => {
-    // Render the parsed feed data in the DOM
-    renderRSSFeed(feed);
-  })
-  .catch(error => {
-    console.error(`Failed to fetch or parse RSS feed: ${error}`);
-  });
-
-
 // generate unique ID for each message div of bot
 // necessary for typing text effect for that specific reply
 // without unique ID, typing text will work on every element
