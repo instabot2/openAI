47411dbe3456c64d60deef83bcf0a8d3903409ea
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-09 10:28:39 +0800

    Update rss.js

--- client/rss.js
+++ client/rss.js
@@ -1,31 +1,10 @@
-// Import the rss-parser library
-import Parser from 'rss-parser';
+// rss.js
+const Parser = require('rss-parser');
+const parser = new Parser();
 
-// Define a function to fetch and parse an RSS feed
-export async function fetchRSSFeed(url) {
-  // Initialize a new instance of the Parser class
-  const parser = new Parser();
-
-  try {
-    // Use the parser to fetch and parse the RSS feed
-    const feed = await parser.parseURL(url);
-
-    // Extract the relevant data from each item in the feed
-    const items = feed.items.map(item => ({
-      title: item.title,
-      link: item.link,
-      description: item.contentSnippet || item.description || '',
-      pubDate: item.pubDate,
-    }));
-
-    // Return the parsed data
-    return {
-      title: feed.title,
-      description: feed.description,
-      items,
-    };
-  } catch (error) {
-    console.error(`Failed to fetch or parse RSS feed at ${url}: ${error}`);
-    return null;
-  }
+async function getFeed() {
+  const feed = await parser.parseURL('https://www.msn.com/en-us/news/rss');
+  return feed.items;
 }
+
+module.exports = getFeed;
