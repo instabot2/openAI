4d7cec58a6f181dac112955d2153da8875c59f66
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-12 13:11:37 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -102,29 +102,6 @@ function chatStripe(isAi, value, uniqueId) {
 
 
 
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('4c7b3dd6ff024a2a878f173ef2391f2f');
-
-async function getFeed(searchQuery) {
-  const options = {
-    q: searchQuery,
-    sources: 'bbc-news,the-verge',
-    category: 'business',
-    language: 'en',
-    country: 'us'
-  };
-
-  try {
-    const response = await newsapi.v2.topHeadlines(options);
-    console.log(response);
-    return response.articles;
-  } catch (error) {
-    console.error('Error fetching news:', error);
-    return [];
-  }
-}
-
-
 
 
 
