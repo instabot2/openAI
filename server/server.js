15baea7f035d5fe042188a2f7e68219dc5930eb0
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-12 15:55:15 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.post('/', async (req, res) => {
     }
 
     const response = await openai.createCompletion({
-      model: 'text-davinci-003',
+      model: 'gpt-3.5-turbo',
       prompt: prompt,
       temperature: 0,
       max_tokens: 3000,
