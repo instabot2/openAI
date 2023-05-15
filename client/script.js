e01da7a4f1493793f6dc55698bc46ce879410158
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-15 12:48:48 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -103,7 +103,8 @@ function chatStripe(isAi, value, uniqueId) {
 
 //const apiUrl = `https://content.guardianapis.com/search?tag=${encodeURIComponent(queryString)}&api-key=35831ef2-e9cf-4977-b5ef-00856e0563c9`;
 function loadData(queryString) {
-  const encodedQuery = "market today";
+  const encodedQuery = encodeURIComponent(queryString);
+  //const encodedQuery = "market today";
   const apiUrl = `https://content.guardianapis.com/search?q=${encodedQuery}&api-key=35831ef2-e9cf-4977-b5ef-00856e0563c9`;
   fetch(apiUrl)
     .then(response => response.json())
