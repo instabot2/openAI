2514b95abbf8459b070c89c3c2d4b30212dee7a1
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-16 12:29:03 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -173,8 +173,8 @@ const handleSubmit = async (e) => {
   //window.alert(`The old messages are: ${JSON.stringify(oldMessages)}`);
   
   // Retrieve stored newsupdate from local storage
-  const NewsUpdate = JSON.parse(localStorage.getItem('metaTextContent')) || [];
-  NewsUpdate.push(meta.textContent);
+  //const NewsUpdate = JSON.parse(localStorage.getItem('metaTextContent')) || [];
+  //NewsUpdate.push(meta.textContent);
   //localStorage.setItem('metaTextContent', JSON.stringify(NewsUpdate));
 
   
